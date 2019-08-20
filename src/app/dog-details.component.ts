9915281234f6fa1144f7d7ea87b6f983b2611786
emaatrb <Mathi018>
2019-08-20 12:00:07 +0200

    adding button

--- src/app/dog-details.component.ts
+++ src/app/dog-details.component.ts
@@ -4,9 +4,14 @@ import {Component, Input} from '@angular/core'
     selector:"dog-details",
     template:`
         <h2>{{name}} - {{color}}</h2>
+        <button (click)="handleClick()">Bark</button>
     `
 })
 export class DogDetailsComponent{
     @Input()name:string
     @Input()color:string
+
+    handleClick(){
+        console.log("Click")
+    }
 }
