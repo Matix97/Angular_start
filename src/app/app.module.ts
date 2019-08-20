f698adcea0017a0d325ad595ec9484510574b06b
emaatrb <Mathi018>
2019-08-20 11:30:46 +0200

    adding first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import{DogDetailsComponent} from './dog-details.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dog-details.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:"dog-details",
+    template:`
+        <h2>Dog Details</h2>
+    `
+})
+export class DogDetailsComponent{
+
+}
