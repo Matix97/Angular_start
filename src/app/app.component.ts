cc7a1b49028659514cf99a689478b25f08ae21f0
emaatrb <Mathi018>
2019-08-20 11:49:30 +0200

    adding iamge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mój początek';
+  catGif = "http://www.pets4homes.co.uk/images/articles/2355/large/burmese-cat-health-and-genetics-547d8a2a0101a.jpg";
+  catGifAlt = "Image with cat";
 }
--- src/app/dog-details.component.ts
+++ src/app/dog-details.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core'
+import {Component, Input} from '@angular/core'
 
 @Component({
     selector:"dog-details",
@@ -7,5 +7,5 @@ import {Component} from '@angular/core'
     `
 })
 export class DogDetailsComponent{
-    name="Mat"
+    @Input()name:string
 }
